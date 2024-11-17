5ac8fdd1538aeb8a4a8bf1a805b7fbd566ba965b
Jhonatan <jhonatan.suardi123@gmail.com>
2024-11-17 17:20:49 -0300

    changing api structure

--- src/main/kotlin/com/john/titanic/presentation/PassengerController.kt
+++ src/main/kotlin/com/john/titanic/presentation/PassengerController.kt
@@ -1,13 +1,13 @@
-package com.john.titanic.presentation
+    package com.john.titanic.presentation
 
-import com.john.titanic.presentation.dto.PassengerDto
-import com.john.titanic.service.PassengerService
-import jakarta.validation.constraints.Max
-import jakarta.validation.constraints.Min
-import org.springframework.data.domain.PageRequest
-import org.springframework.http.ResponseEntity
-import org.springframework.validation.annotation.Validated
-import org.springframework.web.bind.annotation.*
+    import com.john.titanic.presentation.dto.PassengerDto
+    import com.john.titanic.service.PassengerService
+    import jakarta.validation.constraints.Max
+    import jakarta.validation.constraints.Min
+    import org.springframework.data.domain.PageRequest
+    import org.springframework.http.ResponseEntity
+    import org.springframework.validation.annotation.Validated
+    import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/v1/passengers")
@@ -16,31 +16,31 @@ class PassengerController(
     private val passengerService: PassengerService
 ) {
 
-    @GetMapping
-    fun getPassengers(
-        @RequestParam
-        survived: Boolean?,
-        @RequestParam
-        @Max(value = 3, message = "The maximum value for pclass must be at most 3.")
-        @Min(value = 1, message = "The minimum value for pclass must be at most 1.")
-        pclass: Int?,
-        @RequestParam (defaultValue = "0") page: Int,
-        @RequestParam (defaultValue = "5") size: Int
-    ): ResponseEntity<Any> {
+        @GetMapping
+        fun getPassengers(
+            @RequestParam
+            survived: Boolean?,
+            @RequestParam
+            @Max(value = 3, message = "The maximum value for pclass must be at most 3.")
+            @Min(value = 1, message = "The minimum value for pclass must be at most 1.")
+            pclass: Int?,
+            @RequestParam (defaultValue = "0") page: Int,
+            @RequestParam (defaultValue = "5") size: Int
+        ): ResponseEntity<Any> {
 
-        val pageable = PageRequest.of(page, size)
+            val pageable = PageRequest.of(page, size)
 
-        passengerService.getPassengers(survived, pclass, pageable).takeIf { it.passengerDto.isNotEmpty() }
-            ?.run { return ResponseEntity.ok(this) } ?: return ResponseEntity.noContent().build()
+            passengerService.getPassengers(survived, pclass, pageable).takeIf { it.passengerDto.isNotEmpty() }
+                ?.run { return ResponseEntity.ok(this) } ?: return ResponseEntity.noContent().build()
+
+        }
+
+        @GetMapping("/{passenger_id}")
+        fun getPassengerById(
+            @PathVariable("passenger_id") passengerId: Long
+        ): PassengerDto {
+            return passengerService.getPassengersById(passengerId)
+        }
+
 
     }
-
-    @GetMapping("/{passenger_id}")
-    fun getPassengerById(
-        @PathVariable("passenger_id") passengerId: Long
-    ): PassengerDto {
-        return passengerService.getPassengersById(passengerId)
-    }
-
-
-}
